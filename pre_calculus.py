364466992e72686ff75f457638ed6aec54e4248d
eEqualsMxC <justinmustaine@gmail.com>
2021-06-17 16:24:49 -0400

    Cleaned up notes and uneccary code.

--- pre_calculus.py
+++ pre_calculus.py
@@ -37,67 +37,10 @@ def cossine(theta, precision = 20, fp = 10):
     return sine(theta + (PI/2), precision, fp)
 def tangent(theta, precision = 20, fp = 10):
     """Approximate the sin(x)"""
-    return sine(theta, precision, fp) / cossine(theta, precision, fp)
-
-# class Circle(Trig):
-#     """A class for a circle on a cartesian plane"""
-#     def __init__(self, radius: float = None) -> Circle:
-#         super(Circle, self).__init__()
-#         self.radius = radius
-
-    
-    
+    return sine(theta, precision, fp) / cossine(theta, precision, fp) 
 
 """Return a factorial of an integer."""
 fact = lambda x: 1 if x == 0 else x * factorial(x - 1)
-
-
-# def sin(theta: float, )
-
-# print(PI)
-# import math
-# from point import Point
-# p1 = Point(1,2) 
-# p2 = Point(2,3)
-# p3 = Point(3,4)
-
-# def rise(pt1, pt2) -> float:
-#     return pt2.y - pt1.y
-
-# def run(pt1, pt2) -> float:
-#     return pt2.y - pt1.y
-
-# def slope(pt1, pt2):
-#     return rise(pt1,pt2) / run(pt1,pt2)
-
-
-# ad_1 = run(p1, p2)
-# op_1 = rise(p1, p2)
-# h_1 = p1.distance(p2)
-
-
-# # temp_pt = Point(p1.x + run, p1.y)
-
-# dis = p2.distance(p3) # This will be given
-
-# h_2 = h_1 + dis 
-# sin_theta = op_1 / h_1
-# op_2 =  sin_theta * h_2
-# y_coor =  p1.y + op_2
-# ad_2 = ((h_2) ** 2 -(op_2) ** 2) ** 0.5
-# x_coor = p1.x + ad_2
-# fp = Point(x_coor,y_coor)
-# tp = Point(3,4)
-
-
-# print(op_2/h_2)         
-
-
-
-
-
-
-# slp = slope(p1, p3)
 
 
 
